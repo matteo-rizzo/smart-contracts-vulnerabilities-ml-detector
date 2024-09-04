ba99a73d8e45e2ef48713c95c66770783293f037
Matteo Rizzo <matteo.rizzo.phd@gmail.com>
2024-09-04 10:10:38 +0200

    Fixed bug in GCNClassifier.py

--- src/classes/classifiers/GCNClassifier.py
+++ src/classes/classifiers/GCNClassifier.py
@@ -8,33 +8,30 @@ from torch_geometric.nn import GCNConv
 class GCNClassifier(torch.nn.Module):
     def __init__(self, input_size: int, hidden_size: int, output_size: int):
         """
-        Initialize the GCNClassifier.
+        Initialize the GCNClassifier for multilabel classification.
 
         :param input_size: The number of input features.
-        :type input_size: int
         :param hidden_size: The number of hidden units in the hidden layers.
-        :type hidden_size: int
         :param output_size: The number of output classes.
-        :type output_size: int
         """
         super(GCNClassifier, self).__init__()
         self.conv1 = GCNConv(input_size, hidden_size)
         self.bn1 = BatchNorm1d(hidden_size)
         self.conv2 = GCNConv(hidden_size, hidden_size)
         self.bn2 = BatchNorm1d(hidden_size)
-        self.conv3 = GCNConv(hidden_size, output_size)
-        self.bn3 = BatchNorm1d(output_size)
+        self.conv3 = GCNConv(hidden_size, output_size)  # Output layer for multilabel classification
 
     def forward(self, data: Data) -> torch.Tensor:
         """
         Forward pass through the network.
 
         :param data: The input data containing node features and edge indices.
-        :type data: Data
-        :return: The output logits of the network.
-        :rtype: torch.Tensor
+        :return: The output logits of the network for multilabel classification.
         """
         x, edge_index = data.x, data.edge_index
+
+        # Ensure edge_index is of type torch.long (int64)
+        edge_index = edge_index.long()
 
         # First layer
         x = self.conv1(x, edge_index)
@@ -48,8 +45,10 @@ class GCNClassifier(torch.nn.Module):
         x = F.relu(x)
         x = F.dropout(x, p=0.5, training=self.training)
 
-        # Third layer (output layer)
+        # Output layer (raw logits)
         x = self.conv3(x, edge_index)
-        x = self.bn3(x)
 
-        return x
+        # For multilabel classification, we apply sigmoid to get probabilities
+        x = torch.sigmoid(x)
+
+        return x  # Sigmoid output (probabilities between 0 and 1 for each class)
